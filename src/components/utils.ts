3762bcd987f8c46c784ec6c4563af5ff0ef81c91
yesso12 <trimbledustn@gmail.com>
2026-02-25 20:27:05 +0000

    chore: publish greyhour site

--- src/components/utils.ts
+++
@@ -1,9 +0,0 @@
-export function clsx(...parts: Array<string | false | undefined | null>) {
-  return parts.filter(Boolean).join(' ')
-}
-
-export async function fetchJson<T>(path: string): Promise<T> {
-  const res = await fetch(path, { cache: 'no-store' })
-  if (!res.ok) throw new Error(`Failed to load ${path}: ${res.status}`)
-  return res.json() as Promise<T>
-}
